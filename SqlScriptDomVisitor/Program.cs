9e41e56aa934d07f6aa4de3bfb7f42f19222b8ce
gertd <gert.drapers@live.com>
2013-04-08 22:55:45 -0700

    Simple SqlScriptDom visitors example

---
+++ SqlScriptDomVisitor/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Microsoft.SqlServer.TransactSql.ScriptDom;
+
+namespace SqlScriptDomVisitor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Count() < 1)
+            {
+                Console.WriteLine("No input file specified");
+                return;
+            }
+
+            FileInfo input = new FileInfo(args[0]); 
+
+            bool initialQuotedIdentifiers = false;
+            TSqlParser parser = new TSql110Parser(initialQuotedIdentifiers);
+
+            StreamReader sr = input.OpenText();
+            IList<ParseError> errors;
+
+            TSqlFragment fragment = parser.Parse(sr, out errors);
+            sr.Close();
+
+            if (errors.Count > 0)
+            {
+                Console.WriteLine("Parse {0} errors input stream", errors.Count);
+                return;
+            }
+
+            TableVisitor tableVisitor = new TableVisitor();
+            fragment.Accept(tableVisitor);
+
+            ViewVisitor viewVisitor = new ViewVisitor();
+            fragment.Accept(viewVisitor);
+
+            ProcVisitor procVisitor = new ProcVisitor();
+            fragment.Accept(procVisitor);
+
+            foreach (var table in tableVisitor.Nodes)
+            {
+                Console.WriteLine("table {0}.{1}",
+                    table.SchemaObjectName.SchemaIdentifier.Value,
+                    table.SchemaObjectName.BaseIdentifier.Value);
+            }
+
+            foreach(var view in viewVisitor.Nodes)
+            {
+                Console.WriteLine("view {0}.{1}",
+                    view.SchemaObjectName.SchemaIdentifier.Value,
+                    view.SchemaObjectName.BaseIdentifier.Value);
+            }
+
+            foreach(var proc in procVisitor.Nodes)
+            {
+                Console.WriteLine("proc {0}.{1}",
+                    proc.ProcedureReference.Name.SchemaIdentifier.Value,
+                    proc.ProcedureReference.Name.BaseIdentifier.Value);
+            }
+        }
+    }
+    
+    class TableVisitor : TSqlFragmentVisitor
+    {
+        private List<CreateTableStatement> stmts;
+
+        public TableVisitor()
+        {
+            stmts = new List<CreateTableStatement>();
+        }
+
+        public override void Visit(CreateTableStatement node)
+        {
+            base.Visit(node);
+            stmts.Add(node);
+        }
+
+        public List<CreateTableStatement> Nodes
+        {
+            get
+            {
+                return stmts;
+            }
+        }
+    }
+    
+    class ViewVisitor : TSqlFragmentVisitor
+    {
+        private List<CreateViewStatement> stmts;
+
+        public ViewVisitor()
+        {
+            stmts = new List<CreateViewStatement>();
+        }
+
+        public override void Visit(CreateViewStatement node)
+        {
+            base.Visit(node);
+            stmts.Add(node);
+        }
+
+        public List<CreateViewStatement> Nodes
+        {
+            get
+            {
+                return stmts;
+            }
+        }
+    }
+    
+    class ProcVisitor : TSqlFragmentVisitor
+    {
+        private List<CreateProcedureStatement> stmts;
+
+        public ProcVisitor()
+        {
+            stmts = new List<CreateProcedureStatement>();
+        }
+
+        public override void Visit(CreateProcedureStatement node)
+        {
+            base.Visit(node);
+            stmts.Add(node);
+        }
+
+        public List<CreateProcedureStatement> Nodes
+        {
+            get
+            {
+                return stmts;
+            }
+        }
+    }
+}
---
+++ SqlScriptDomVisitor/test.sql
@@ -0,0 +1,20 @@
+﻿create table dbo.t1
+(
+	c1 int not null identity(1,1) primary key clustered,
+	c2 nvarchar(10) null,
+	c3 datetime default getdate()
+)
+go
+
+create view dbo.v1
+as
+select c1, c3, c2 from t1 where c3 < getdate()
+go
+
+create proc dbo.p1
+as
+begin
+	set nocount on
+	select * from v1
+end
+go
